7127ed9f56e2bdca3cce37e6c394f503c924cc4f
Tafarel Mello <tafarel.mello@gmail.com>
2021-01-19 22:23:34 -0300

    test: ensure EmailValidatorAdapter calls correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -22,4 +22,12 @@ describe('EmailVAlidator Adapter', () => {
 
     expect(isValid).toBe(true)
   })
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter()
+    const isEmailSpy = jest.spyOn(validator, 'isEmail')
+    sut.isValid('any_email@gmail.com')
+
+    expect(isEmailSpy).toHaveBeenCalledWith('any_email@gmail.com')
+  })
 })
