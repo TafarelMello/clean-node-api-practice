717bb1f593823f796fa47ce3fc0a33411f1a01b5
Tafarel Mello <tafarel.mello@gmail.com>
2021-01-19 22:14:55 -0300

    feat: ensure EmailValidatorAdapter return true if validator return true

--- src/utils/email-validator-adapter.ts
+++ src/utils/email-validator-adapter.ts
@@ -1,7 +1,8 @@
 import { IEmailValidator } from '../presentation/protocols/iemail-validator'
+import validator from 'validator'
 
 export class EmailValidatorAdapter implements IEmailValidator {
   isValid (email: string): boolean {
-    return false
+    return validator.isEmail(email)
   }
 }
